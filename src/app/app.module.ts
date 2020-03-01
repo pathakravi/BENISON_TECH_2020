00fb7ec15e41d75170b39e9c58673589c562a58c
pathakravi <ravipathak0804@gmail.com>
2020-03-01 11:08:40 +0530

    1st Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { FeedbackListComponent } from './feedback-list/feedback-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmployeeDetailsComponent,
+    FeedbackListComponent
   ],
   imports: [
     BrowserModule
